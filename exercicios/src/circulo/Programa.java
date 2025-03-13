fff75623802dfd7b7fd328625ac0112cd3127882
Daniel Teixeira <127428783+DanielJT20@users.noreply.github.com>
2025-03-13 11:32:38 -0300

    ajuste

--- exercicios/src/circulo/Programa.java
+++ exercicios/src/circulo/Programa.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
+
         double perimetro, raio, area;
-        System.out.println("Insira o raio de um circulo: ");
+
+        System.out.println("Informe o raio do circulo: ");
+
         raio = teclado.nextFloat();
+
         perimetro = ((2 * Math.PI) * raio);
         area = (Math.PI * Math.pow(raio,2));
+
         System.out.println("O perímetro é : " + perimetro + "\n A área é: " + area);
+
     }
 }
